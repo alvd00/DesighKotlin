e032df2c8c570cb6e96f41fa7aa29f125dfc03cb
Александр Вдовченко <alvd00@mail.ru>
2021-09-10 22:11:32 +0300

    Попытка первого получения картинки

--- app/src/main/java/com/example/desighkotlin/repository/PODAPI.kt
+++ app/src/main/java/com/example/desighkotlin/repository/PODAPI.kt
@@ -1,10 +1,11 @@
 package com.example.desighkotlin.repository
 
+import com.example.desighkotlin.BuildConfig
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-const val API_KEY = "api key"
+const val API_KEY = BuildConfig.NASA_API_KEY
 
 interface PODAPI {
     @GET("planetary/apod")
